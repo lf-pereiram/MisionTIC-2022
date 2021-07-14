a538eca6bc257f9ded69b78ff0a82f7115e4ad36
Luisa Pereira <lufe.pereira23@gmail.com>
2021-07-13 21:59:44 -0500

    Activity1
    First activity in unit 1

---
+++ Ciclo 2/java/Actividades/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package holamundo;
+
+/**
+ *
+ * @author luisa pereira
+ */
+public class HolaMundo {
+
+    //funcion para hacer el llamado Hola Mundo
+    public String mensaje(){
+        String str = new String("Hola mundo");
+        return str;
+        
+    }   
+}
